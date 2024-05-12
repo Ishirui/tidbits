314c0c37b007010242b08c85f05bfc1d866b2b77
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2024-05-13 01:12:23 +0200

    Add zsh settings to `dotfiles/`

--- dotfiles/update.sh
+++ dotfiles/update.sh
@@ -5,3 +5,8 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # VSCode settings.json
 cp "$HOME/.config/Code/User/settings.json" "$SCRIPT_DIR/vscode/settings.json"
+
+# ZSH Settings
+ZSHDIR=${ZDOTDIR:-$HOME}
+cp "$ZSHDIR/.zshrc" "$SCRIPT_DIR/zsh/.zshrc"
+cp "$ZSHDIR/.p10k.zsh" "$SCRIPT_DIR/zsh/.p10k.zsh"
