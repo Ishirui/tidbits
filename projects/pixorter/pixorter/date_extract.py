607ae8765c1a06b569126862d7f4a5289296cf54
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2024-05-14 23:07:04 +0200

    feat(pixorter): Setup EXIF-based datetime extraction

--- projects/pixorter/pixorter/date_extract.py
+++ projects/pixorter/pixorter/date_extract.py
@@ -8,7 +8,14 @@ Module containing different methods for extracting a picture's creation date:
 import re
 from datetime import datetime
 
+from PIL.Image import ExifTags, Image
 from pixorter import FILENAME_REGEXES
+
+DATETIME_TAGS = {
+    name: id
+    for id, name in ExifTags.TAGS.items()
+    if name in ("DateTime", "DateTimeOriginal", "DateTimeDigitized")
+}
 
 
 class NoMatchException(Exception):
@@ -32,3 +39,21 @@ def get_date_from_filename(filename: str) -> datetime:
         if match.group(k)
     }
     return datetime(**datetime_parts)  # type: ignore
+
+
+def get_date_from_exif(image: Image) -> datetime:
+    """Try to get datetime info from the image's EXIF metadata"""
+    exif = image.getexif()
+
+    if exif is None:
+        raise NoMatchException()
+
+    datetime_str = None
+    for tag_id in DATETIME_TAGS.values():
+        datetime_str = datetime_str or exif.get(tag_id, default=None)
+
+    if datetime_str is None:
+        raise NoMatchException()
+
+    # See https://stackoverflow.com/a/62077871
+    return datetime.strptime(datetime_str, "%Y:%m:%d %H:%M:%S")
