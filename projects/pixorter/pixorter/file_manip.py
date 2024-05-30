f1ac7c1284b7d46f1c18140212441de30fc23e41
Pierre-Louis Veyrenc <veyrencpierrelouis@gmail.com>
2024-05-30 16:40:39 +0200

    refactor(pixorter): Use common checking function for all operations
    Also implement creating top-directory structure upon operation

--- projects/pixorter/pixorter/file_manip.py
+++ projects/pixorter/pixorter/file_manip.py
@@ -14,53 +14,44 @@ def walk_dir(directory: Path) -> Generator[Path, None, None]:
             yield folder / file
 
 
-def move(src_file: Path, dest_file: Path):
-    if not src_file.is_file() or not dest_file.is_file():
+def op_common(src_file: Path, dest_file: Path):
+    """Common things to do for every operation type"""
+    if dest_file.exists():
+        raise ValueError(f"Destination file {dest_file} already exists !")
+
+    if not src_file.is_file() or dest_file.is_file():
         raise ValueError(
-            f"Only allowing moves between files. Offending: {src_file:s} -> {dest_file}"
+            f"Only allowing operations between files. Offending: {src_file} -> {dest_file}"
         )
 
+    # Make sure the parent directories tree exist
+    dest_file.parent.mkdir(parents=True, exist_ok=True)
+
+
+def move(src_file: Path, dest_file: Path):
+    op_common(src_file, dest_file)
     logging.info("Moving %s -> %s...", src_file, dest_file, extra=_CURR_PICTURE)
     shutil.move(src_file, dest_file)
 
 
 def copy(src_file: Path, dest_file: Path):
-    if not src_file.is_file() or not dest_file.is_file():
-        raise ValueError(
-            f"Only allowing copies between files. Offending: {src_file} -> {dest_file}"
-        )
-
+    op_common(src_file, dest_file)
     logging.info("Copying %s |-> %s...", src_file, dest_file, extra=_CURR_PICTURE)
     shutil.copy(src_file, dest_file)
 
 
 def symlink(src_file: Path, dest_file: Path):
-    if not src_file.is_file() or not dest_file.is_file():
-        raise ValueError(
-            f"Only allowing symlink creation between files. Offending: {src_file} -> {dest_file}"
-        )
-
+    op_common(src_file, dest_file)
     logging.info("Symlinking %s <- %s...", src_file, dest_file, extra=_CURR_PICTURE)
     dest_file.symlink_to(src_file)
 
 
 def hardlink(src_file: Path, dest_file: Path):
-    if not src_file.is_file() or not dest_file.is_file():
-        raise ValueError(
-            f"Only allowing hardlinking between files. Offending: {src_file} -> {dest_file}"
-        )
-
+    op_common(src_file, dest_file)
     logging.info("Hardlinking %s <-| %s...", src_file, dest_file, extra=_CURR_PICTURE)
     dest_file.hardlink_to(src_file)
 
 
 def dry_run(src_file: Path, dest_file: Path):
-    if not src_file.is_file() or not dest_file.is_file():
-        logging.error(
-            "Would throw an error ! %s or %s is not a file.",
-            src_file,
-            dest_file,
-            extra=_CURR_PICTURE,
-        )
-
+    op_common(src_file, dest_file)
     logging.info("Running operation %s -> %s", src_file, dest_file, extra=_CURR_PICTURE)
